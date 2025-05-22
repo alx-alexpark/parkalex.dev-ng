968878feb280f44dc8c258d742c393d5709eb876
Alex Park <me@parkalex.dev>
2025-05-22 13:00:34 -0500

    new colors :3

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE: Site = {
   desc: "My personal website",
   title: "Alex Park",
   ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
+  lightAndDarkMode: false,
   postPerPage: 10,
 };
 
